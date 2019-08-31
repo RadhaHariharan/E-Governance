ea245ec410a929cc0524d88b143faa6935e3ae74
RadhaHariharan <42894756+RadhaHariharan@users.noreply.github.com>
2019-08-31 13:31:05 +0530

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,5 @@
-const smallmenuicon = document.getElementById("menusmall-icon");
-const hide = document.getElementById("hide1");
 const menuicon = document.getElementById("menu-icon");
 const slideoutmenu = document.getElementById("slideout-menu");
-
-
-smallmenuicon.addEventListener('click',function() {
-    if(hide.style.opacity=='1'){
-        hide.style.opacity='0';
-        hide.style.pointerEvents = 'none';
-    }
-    else {
-        hide.style.opacity='1';
-        hide.style.pointerEvents = 'auto';   
-    }
-});
 
 menuicon.addEventListener('click',function() {
     if(slideoutmenu.style.opacity=='1'){
